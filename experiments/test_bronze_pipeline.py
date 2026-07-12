b09cbec6c324ec33bc0206fff071fc82b125baa5
Lucky Jha <jhalucky61@gmail.com>
2026-07-12 21:30:52 +0530

    developed and tested primary key validator successfully

--- experiments/test_bronze_pipeline.py
+++ experiments/test_bronze_pipeline.py
@@ -1,8 +1,9 @@
 from src.bronze.bronze_pipeline import load_bronze_tables
 from src.spark.spark_session import get_spark_session
 from src.validation.null_validator import validate_nulls
-# from src.validation.duplicate_validator import validate_duplicates
+from src.validation.duplicate_validator import validate_duplicates
 from src.validation.schema_validator import schema_validator
+from src.validation.primary_key_validator import validate_duplicate_primary_keys
 from schemas import CUSTOMER_SCHEMA
 
 spark = get_spark_session()
@@ -16,16 +17,15 @@ orders = tables["orders"]
 # null_report = validate_nulls(customers)
 # null_report = validate_nulls(orders)
 # duplicate_report = drop_duplicates(customers)
-schema_report = schema_validator(customers, CUSTOMER_SCHEMA)
+# schema_report = schema_validator(customers, CUSTOMER_SCHEMA)
+primary_key_report = validate_duplicate_primary_keys(customers, "customer_id")
 
+# for name, df in tables.items():
+#     print("="*50)
+#     print(name.upper())
+#     df.printSchema()
+#     df.show(5)
 
-for name, df in tables.items():
-    print("="*50)
-    print(name.upper())
-    df.printSchema()
-    df.show(5)
-
-print(f"Schema test passed or failed: {schema_report}")
 
 
     
