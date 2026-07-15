e7b36ae35eaa54bd622ed38b2263c7b27ccc33bf
Lucky Jha <jhalucky61@gmail.com>
2026-07-15 18:17:54 +0530

    developed customer analytics and tested it

---
+++ src/published/customer_analytics.py
@@ -0,0 +1,26 @@
+from src.published.read_gold import read_gold
+from src.transformations.write_parquet import write_parquet
+from src.spark.spark_session import get_spark_session
+
+
+from src.config.aws_config import PUBLISHED_PATH
+
+def publish_customer_analytics(spark):
+
+    customer_gold = read_gold(
+        spark, "customer_analytics"
+    )
+
+    write_parquet(
+        customer_gold,
+        PUBLISHED_PATH+"customer_analytics"
+    )
+
+
+
+    print("Customer analytics published.")
+
+
+if __name__ == "__main__":
+    spark = get_spark_session()
+    publish_customer_analytics(spark)
