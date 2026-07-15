2bc69ca54268d2924f87336c4c436a58f8c83f39
Lucky Jha <jhalucky61@gmail.com>
2026-07-15 18:46:37 +0530

    published product analytics done

--- src/published/product_analytics.py
+++ src/published/product_analytics.py
@@ -1,7 +1,7 @@
 from src.spark.spark_session import get_spark_session
 from src.published.read_gold import read_gold
 from src.transformations.write_parquet import write_parquet
-from src.config.aws_config import GOLD_PATH
+from src.config.aws_config import PUBLISHED_PATH
 
 def publish_build_analytics(spark):
 
@@ -12,7 +12,12 @@ def publish_build_analytics(spark):
 
     write_parquet(
         product_gold,
-        GOLD_PATH+"product_analytics"
+        PUBLISHED_PATH+"product_analytics"
     )
 
     print("Product analytics published")
+
+
+if __name__ == "__main__":
+    spark = get_spark_session()
+    publish_build_analytics(spark)
