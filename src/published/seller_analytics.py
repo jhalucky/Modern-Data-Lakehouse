13754e2f584f794252a10475361eb7a5de501f9f
Lucky Jha <jhalucky61@gmail.com>
2026-07-15 18:55:35 +0530

    Sales analytics published

---
+++ src/published/seller_analytics.py
@@ -0,0 +1,26 @@
+from src.spark.spark_session import get_spark_session
+
+from src.transformations.write_parquet import write_parquet
+from src.published.read_gold import read_gold
+
+from src.config.aws_config import PUBLISHED_PATH
+
+def publish_seller_analytics(spark):
+
+    seller_gold = read_gold(
+        spark,
+        "seller_analytics"
+    )
+
+    write_parquet(
+        seller_gold,
+        PUBLISHED_PATH+"seller_analytics"
+    )
+
+    print("Seller analysis published!")
+
+
+
+if __name__ == "__main__":
+    spark = get_spark_session()
+    publish_seller_analytics(spark)
